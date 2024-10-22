f3822a78f32ea81dd0686712d17e3a368491aff4
Varinolav <pavivand@gmail.com>
2024-10-22 12:46:22 +0300

    inf lab 3 done

--- Информатика/Лабы/lab 3/task 3.py
+++ Информатика/Лабы/lab 3/task 3.py
@@ -1,21 +1,18 @@
 # 466985 % 5 = 0
 import re
 
-regexp = re.compile("[0-9a-z._]+@[a-z]+\\.[a-z]+", re.IGNORECASE)
+regexp = re.compile("[0-9a-z._]+@([a-z]+\\.[a-z]+)", re.IGNORECASE)
 
 
 def test(f):
     if not re.match(regexp, f):
         print('Fail!')
         return
-    else:
-        print(f.split('@')[-1])
+    print(re.findall(regexp, f)[0])
 
 
-test('morj@gmail.com') # gmail.com
-test('morj@gmailcom') # Fail!
-test('morj@yandex.ru') # yandex.ru
-test('morj@gmail.') # Fail!
-test('100mo_r.j@gmail.com') # gmail.com
-
-
+test('morj@gmail.com')  # gmail.com
+test('morj@gmailcom')  # Fail!
+test('morj@yandex.ru')  # yandex.ru
+test('morj@gmail.')  # Fail!
+test('100mo_r.j@gmail.com')  # gmail.com
